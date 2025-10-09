22ac4a0f68f99877ceb9be2bf52eac86bbf212ed
Theo Slater <155472213+TheoSlater@users.noreply.github.com>
2025-10-09 16:52:31 +0100

    Add Google site verification meta tag and HTML file

--- app/page.tsx
+++ app/page.tsx
@@ -11,26 +11,32 @@ import { ScrollIndicator } from "@/components/ScrollIndicator";
 
 export default function Home() {
   return (
-    <Stack
-      component="main"
-      direction="column"
-      sx={{
-        bgcolor: "background.default",
-        color: "text.primary",
-        fontWeight: 100,
-        overflowX: "hidden",
-      }}
-    >
-      <ScrollIndicator />
-      <FadeIn delay={0.2} direction="up">
-        <Hero />
-      </FadeIn>
-      <Philosophy />
-      <ScaleOnScroll scaleStart={0.8} scaleEnd={1}>
-        <Products />
-      </ScaleOnScroll>
-      <Future />
-      <CallToAction />
-    </Stack>
+    <>
+      <meta
+        name="google-site-verification"
+        content="00tJU0St__ZYIm1X1eljrVi2yo6OTnjz0KXIczQXmos"
+      />
+      <Stack
+        component="main"
+        direction="column"
+        sx={{
+          bgcolor: "background.default",
+          color: "text.primary",
+          fontWeight: 100,
+          overflowX: "hidden",
+        }}
+      >
+        <ScrollIndicator />
+        <FadeIn delay={0.2} direction="up">
+          <Hero />
+        </FadeIn>
+        <Philosophy />
+        <ScaleOnScroll scaleStart={0.8} scaleEnd={1}>
+          <Products />
+        </ScaleOnScroll>
+        <Future />
+        <CallToAction />
+      </Stack>
+    </>
   );
 }
